e3cc727f13aaeb0fa9421bf2d886e7868ec16a85
Ch Ammar <96951639+chammar55@users.noreply.github.com>
2022-12-10 18:37:02 +0500

    destini

---
+++ android/app/src/main/kotlin/com/example/destini_challange_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.destini_challange_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/story.dart
@@ -0,0 +1,17 @@
+//TODO: Step 2 - Create a new class called Story.
+class Story {
+  late String storyTitle;
+  late String choice1;
+  late String choice2;
+  Story(
+      {required String storyTitle,
+      required String choice1,
+      required String choice2}) {
+    this.storyTitle = storyTitle;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
+  }
+}
+//TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
+
+//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
